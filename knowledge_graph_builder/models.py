2b62f4d6b6e5d8ccb85ce567db91b3dc06912ccd
Jeongmin Byun <jmbyun91@gmail.com>
2018-09-27 15:09:47 +0900

    improve models to handle additional data

--- knowledge_graph_builder/models.py
+++ knowledge_graph_builder/models.py
@@ -1,16 +1,20 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Float, Text, create_engine
+from sqlalchemy import Column, ForeignKey, Boolean, Integer, String, Float, Text, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
+
 from . import config
+
 
 Base = declarative_base()
 
 class Item(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
-    id_str = Column(String(250), unique=True)
-    classname = Column(String(250), nullable=False)
-    value = Column(Text)
+    id_str = Column(String(250), unique=True, nullable=True)
+    classname = Column(String(250), default='', nullable=False)
+    label = Column(Text, default='')
+    value = Column(Text, default='')
+    is_abstract = Column(Boolean, default=False)
 
 class ValidTime(Base):
     __tablename__ = 'valid_time'
@@ -36,23 +40,23 @@ class Property(Base):
     id = Column(Integer, primary_key=True)
     id_str = Column(String(250), unique=True)
     classname = Column(String(250), nullable=False)
-    value = Column(Text)
+    value = Column(Text, default='')
     time_start = Column(Float)
     time_end = Column(Float)
     source_item_id = Column(Integer, ForeignKey('item.id'))
     target_item_id = Column(Integer, ForeignKey('item.id'))
+    relation_item_id = Column(Integer, ForeignKey('item.id'))
     source = relationship(Item, foreign_keys=[source_item_id])
     target = relationship(Item, foreign_keys=[target_item_id])
+    relation = relationship(Item, foreign_keys=[relation_item_id])
 
-engine = create_engine(config.SQLALCHEMY_ENGINE)
+def get_engine():
+    return create_engine(config.SQLALCHEMY_ENGINE)
 
 def init_db():
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(get_engine())
 
 def get_sessionmaker():
+    engine = get_engine()
     Base.metadata.bind = engine
     return sessionmaker(bind=engine)
-
-
-
-    
